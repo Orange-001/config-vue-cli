6fead3548a313734735362ed4983076f1b33d4bc
Orange-001 <1347680852@qq.com>
2023-03-28 21:35:55 +0800

    feat: 新增现代构建模式&gitHooks配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  /**
+   * Type: boolean | Array<string | RegExp>
+     Default: false
+     默认情况下 babel-loader 会忽略所有 node_modules 中的文件。你可以启用本选项，以避免构建后的代码中出现未转译的第三方依赖。
+   */
   transpileDependencies: true
 })
